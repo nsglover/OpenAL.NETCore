94d784acb6e5c7c4352abe668680beb65d286900
Nathaniel Glover <nathanielsglover@gmail.com>
2018-07-08 17:16:13 -0400

    Add simple context creation example

---
+++ Examples/src/ContextCreation.cs
@@ -0,0 +1,15 @@
+﻿using OpenAL;
+
+namespace Examples
+{
+    public class ContextCreation
+    {
+        public static void Main(string[] args)
+        {
+            var device = Alc.OpenDevice(null);
+            var context = Alc.CreateContext(device, null);
+
+            Alc.MakeContextCurrent(context);
+        }
+    }
+}
